b4ca7b80c148dd59698183c8e5afe8e267f25a58
Liliane Rodrigues <liliane_pdf@hotmail.com>
2018-08-14 14:28:18 -0300

    alterações realizadas nas telas de login

--- src/controller/TelaLoginController.java
+++ src/controller/TelaLoginController.java
@@ -32,16 +32,22 @@ public class TelaLoginController implements Initializable {
 
     @FXML
     private void cliqueBotaoEntrar(ActionEvent event) throws Exception {
-     if (Usuario.login(campoLogin.getText().trim(), campoSenha.getText().trim())){
+        if (Usuario.login(campoLogin.getText().trim(), campoSenha.getText().trim())){
+            ChangeScreen change = new ChangeScreen();
+
+            Stage mainStage = change.change(event, Caminho.telaGerente, Especificacoes.getSoftwareNome(), true);
+            mainStage.show();
+        }
+        else if (Usuario.loginFunc(campoLogin.getText().trim(), campoSenha.getText().trim())){
             ChangeScreen change = new ChangeScreen();
 
             Stage mainStage = change.change(event, Caminho.telaFunc, Especificacoes.getSoftwareNome(), true);
             mainStage.show();
-         }
-	     else {
-	         mensagemErro.setVisible(true);
-	         mensagemErro.setText("Login e/ou senha inv·lido(s)");
-	     }
+        }
+        else {
+            mensagemErro.setVisible(true);
+            mensagemErro.setText("Login e/ou senha inv√°lido(s)");
+        }
     }
     
 }
--- src/model/Usuario.java
+++ src/model/Usuario.java
@@ -6,10 +6,18 @@ public class Usuario {
 
     private static final String login = "admin";
     private static final String senha = "admin";
-
+    
+    private static final String loginFunc = "func";
+    private static final String senhaFunc = "func";
+    
     public static boolean login (String log, String pass){
 
         return (login.equals(log) && senha.equals(pass));
+
+    }
+    public static boolean loginFunc (String log, String pass){
+
+        return (loginFunc.equals(log) && senhaFunc.equals(pass));
 
     }
 
