0ed1d3fd3190d7f18bf21884c3cdc376ab6f4850
anderson039703 <andersonfernandespinheiro@gmail.com>
2018-09-10 22:41:27 -0300

    Login

--- src/controller/TelaLoginController.java
+++ src/controller/TelaLoginController.java
@@ -1,6 +1,8 @@
 package controller;
 
+import database.DAOs.FuncionarioDAO;
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -16,6 +18,7 @@ import model.Caminho;
 import model.ChangeScreen;
 import model.Especificacoes;
 import model.Usuario;
+import model.entities.Funcionario;
 
 public class TelaLoginController implements Initializable {
 
@@ -34,21 +37,30 @@ public class TelaLoginController implements Initializable {
 
     @FXML
     private void cliqueBotaoEntrar(ActionEvent event) throws Exception {
-        if (Usuario.login(campoLogin.getText().trim(), campoSenha.getText().trim())){
-            ChangeScreen change = new ChangeScreen();
+        FuncionarioDAO dao = new FuncionarioDAO();
+        List<Funcionario> fun = dao.all();
+        
+        for(int x =0; x<fun.size() ; x++){
+            if (Usuario.login(campoLogin.getText().trim(), campoSenha.getText().trim())){
+                x = fun.size();
+                ChangeScreen change = new ChangeScreen();
 
-            Stage mainStage = change.change(event, Caminho.telaGerente, Especificacoes.getSoftwareNome(), true);
-            mainStage.show();
-        }
-        else if (Usuario.loginFunc(campoLogin.getText().trim(), campoSenha.getText().trim())){
-            ChangeScreen change = new ChangeScreen();
+                Stage mainStage = change.change(event, Caminho.telaGerente, Especificacoes.getSoftwareNome(), true);
+                mainStage.show();
+            }
+            else if (campoLogin.getText().equals(fun.get(x).getLogin()) && campoSenha.getText().equals(fun.get(x).getSenha())){
+                x = fun.size();
+                ChangeScreen change = new ChangeScreen();
 
-            Stage mainStage = change.change(event, Caminho.telaFunc, Especificacoes.getSoftwareNome(), true);
-            mainStage.show();
-        }
-        else {
-            mensagemErro.setVisible(true);
-            mensagemErro.setText("Login e/ou senha inválido(s)");
+                Stage mainStage = change.change(event, Caminho.telaFunc, Especificacoes.getSoftwareNome(), true);
+                mainStage.show();
+            }
+            else {
+                if(x == fun.size()-1){
+                mensagemErro.setVisible(true);
+                mensagemErro.setText("Login e/ou senha inválido(s)");
+                }
+            }
         }
     }
 
