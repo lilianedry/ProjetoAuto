8e938af8007119aa9cf0e1a8efb6d098671d9fd4
anderson039703 <andersonfernandespinheiro@gmail.com>
2018-09-14 18:09:29 -0300

    TableView e Devolver veiculo

--- src/controller/TelaLocarVeiculosController.java
+++ src/controller/TelaLocarVeiculosController.java
@@ -8,13 +8,20 @@ package controller;
 import controller.alerts.Alertas;
 import database.DAOs.CarroDAO;
 import database.DAOs.ClienteDAO;
+import database.DAOs.SolicitaCarroDAO;
 import java.net.URL;
 import java.time.LocalDate;
 import java.time.ZoneId;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -23,12 +30,14 @@ import javafx.scene.control.DatePicker;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 import model.Caminho;
 import model.ChangeScreen;
 import model.Especificacoes;
 import model.entities.Carro;
 import model.entities.Cliente;
+import model.entities.Funcionario;
 import model.entities.relationships.SolicitaCarro;
 
 public class TelaLocarVeiculosController implements Initializable {
@@ -62,16 +71,35 @@ public class TelaLocarVeiculosController implements Initializable {
     private Button voltar;
     @FXML
     private TextField campoValor;
+    
     @FXML
-    private TableView<?> listaAlunos;
+    private TableView<Cliente> listaAlunos;
+
     @FXML
-    private TableColumn<?, ?> colunaNome;
+    private TableColumn<Cliente, String> colunaNome;
+
     @FXML
-    private TableColumn<?, ?> colunaCPF;
+    private TableColumn<Cliente, String> colunaCPF;
+    
+    private SolicitaCarro sele;
+    
+    private ObservableList<Cliente> cliente = FXCollections.observableArrayList();
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+       initTable();
+       
+       listaAlunos.getSelectionModel().selectedItemProperty().addListener(new ChangeListener() {
+            @Override
+            public void changed(ObservableValue observable, Object oldValue, Object newValue) {
+               Cliente selecionado = (Cliente) newValue;  
+               SolicitaCarro soli = new SolicitaCarro();
+               soli.setCliente(selecionado);
+               SolicitaCarroDAO car= new SolicitaCarroDAO();
+               sele = car.selectParam(soli).get(0);
+               System.out.println(selecionado.toString());
+            }
+        });
     }    
 
     @FXML
@@ -95,9 +123,9 @@ public class TelaLocarVeiculosController implements Initializable {
         Date nasc = Date.from(data.atStartOfDay(ZoneId.systemDefault()).toInstant());
         sC.setDataRetirada(nasc);
         
-        LocalDate data1 = campoDataEntrega.getValue();
-        Date nasc1 = Date.from(data1.atStartOfDay(ZoneId.systemDefault()).toInstant());
-        sC.setDataEntrega(nasc1);
+        //LocalDate data1 = campoDataEntrega.getValue();
+        //Date nasc1 = Date.from(data1.atStartOfDay(ZoneId.systemDefault()).toInstant());
+        //sC.setDataEntrega(nasc1);
         
         LocalDate data2 = campoPrazoFinal.getValue();
         Date nasc2 = Date.from(data2.atStartOfDay(ZoneId.systemDefault()).toInstant());
@@ -108,7 +136,10 @@ public class TelaLocarVeiculosController implements Initializable {
         cli.getSolicitaCarro().add(sC);
         
         ClienteDAO cliDAO3 = new ClienteDAO();
-        cliDAO3.update(cli);        
+        cliDAO3.update(cli); 
+        
+        
+        listaAlunos.setItems(atualizaTabela());
         
         Alertas.mostraAlertaInfo("Locação de Veículo", "Locação realizada com sucesso!");
 	
@@ -116,10 +147,14 @@ public class TelaLocarVeiculosController implements Initializable {
 
     @FXML
     private void devolveVeiculo(ActionEvent event) {
-        /*SolicitaCarro sol = new SolicitaCarro();
+        sele.setAtivo(false);
+        LocalDate data1 = campoDataEntrega.getValue();
+        Date nasc1 = Date.from(data1.atStartOfDay(ZoneId.systemDefault()).toInstant());
+        sele.setDataEntrega(nasc1);
+        SolicitaCarroDAO dao= new SolicitaCarroDAO();
+        dao.update(sele);
         
-        sol.setValor(campoValor.getText()); // inserir valor somente na entrega*/
-        
+        listaAlunos.setItems(atualizaTabela());
         Alertas.mostraAlertaInfo("Devolução", "Devolução realizada com sucesso!");		
     }
 
@@ -150,8 +185,33 @@ public class TelaLocarVeiculosController implements Initializable {
 
     @FXML
     private void btPesquisa(ActionEvent event) {
+       listaAlunos.setItems(busca());
+    }
+    
+    private ObservableList<Cliente> busca(){
+        ObservableList<Cliente> cliPesquisa = FXCollections.observableArrayList();
+        for(int x = 0; x<cliente.size(); x++){
+            if(cliente.get(x).getNome().contains(campoPesquisa.getText())){
+                cliPesquisa.add(cliente.get(x));
+            }
+        }
+        return cliPesquisa;
     }
 
-
+    public void initTable(){
+        colunaNome.setCellValueFactory(new PropertyValueFactory("nome"));
+        colunaCPF.setCellValueFactory(new PropertyValueFactory("cpf"));
+        listaAlunos.setItems(atualizaTabela());
+    }
+    
+    public ObservableList<Cliente> atualizaTabela(){
+        SolicitaCarroDAO dao = new SolicitaCarroDAO();
+       // List<Cliente> cli = new ArrayList<Cliente>();
+        List<SolicitaCarro> soli = dao.all();
+        for(int x=0; x<soli.size();x++){
+            cliente.add(soli.get(x).getCliente());
+        }
+        return FXCollections.observableArrayList(cliente);
+    }
     
 }
